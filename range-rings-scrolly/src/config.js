53699e56564bf561e75ebf7530c96894f6556e97
Serven Maraghi <maraghiserven@gmail.com>
2021-05-06 12:49:12 -0400

    Add storyboard descriptions

--- range-rings-scrolly/src/config.js
+++ range-rings-scrolly/src/config.js
@@ -17,7 +17,7 @@ var config = {
             image: '',
             description: `China controls 4 bases with runways in the SCS.`,
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 3.50,
                 pitch: 0.00,
                 bearing: 0.00
@@ -35,7 +35,7 @@ var config = {
             image: '',
             description: `The range of combat aircraft from these islands is quite far.`,
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 3.50,
                 pitch: 0.00,
                 bearing: 0.00
@@ -60,7 +60,7 @@ var config = {
             image: '',
             description: `But in reality, combat aircraft are limited to operating within radar range. Without external radar coverage, they have limited awareness of their surroundings and would be vulnerable to attack.`,
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 3.50,
                 pitch: 0.00,
                 bearing: 0.00
@@ -86,7 +86,7 @@ var config = {
             image: '',
             description: `KJ-500 sensing aircraft launched from the islands have a wider sensing range than any radar facilities on the islands themselves. A KJ-500, shown here above mischief reef can sense X miles around it.`,
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 4.00,
                 pitch: 0.00,
                 bearing: 0.00
@@ -110,12 +110,12 @@ var config = {
         },
         {
             id: 'counties100b',
-            title: '',
+            title: ' [KJ-500 maximum range(F)]',
             subtitle: '',
             image: '',
-            description: ``,
+            description: `The KJ-500 has a huge maximum travel range 3000km…`,
             location: {
-                center: [-83.265937, 37.870455],
+                center: [110.09, 22.30],
                 zoom: 5.13,
                 pitch: 22.50,
                 bearing: -26.92,
@@ -136,12 +136,12 @@ var config = {
         },
         {
             id: 'manuDecline3',
-            title: ``,
+            title: `[mischief reef SAM range(G)]`,
             subtitle: '',
             image: '',
-            description: ``,
+            description: `But, like the fighters, they are limited to operating within only a fraction of it. This is because they need to be covered by SAM and ASMs to defend them against potential attack.`,
             location: {
-                center: [-83.066270, 35.627624],
+                center: [110.09, 22.30],
                 zoom: 6,
                 pitch: 60,
                 bearing: -19.72,
@@ -161,12 +161,12 @@ var config = {
         },
         {
             id: 'manuDecline4',
-            title: ``,
+            title: `[KJ-500 sensing range from island SAMS(c)]`,
             subtitle: '',
             image: '',
-            description: ``,
+            description: `This range, the estimated sensing range of KJ-500 aircraft operating within SAM coverage from the islands, represents the practical limit of power projection from the islands themselves. Combat aircraft wouldn’t be able to operate outside of this range without incurring extreme risk. `,
             location: {
-                center: [-83.066270, 35.627624],
+                center: [110.09, 22.30],
                 zoom: 6,
                 pitch: 60,
                 bearing: -19.72,
@@ -186,12 +186,12 @@ var config = {
         },
         {
             id: 'services5',
-            title: '',
+            title: '[carrier group near natuns. Sam range, asm range, sensing range]',
             subtitle: '',
             image: '',
-            description: ``,
+            description: `China’s aircraft carriers have the potential to unlock some of these limitations and extend China’s power projection range much further. A carrier group, including the carrier itself, bringing X number of X fighters, but also Y ship and Z has its own missile capabilities and its own sensing capabilities.`,
             location: {
-                center: [-83.677347, 37.629440],
+                center: [110.09, 22.30],
                 zoom: 5.23,
                 pitch: 52.00,
                 bearing: 13.88,
@@ -212,12 +212,12 @@ var config = {
         },
         {
             id: 'services6',
-            title: 'Overall Loss',
+            title: '[divert airfields]',
             subtitle: '',
             image: '',
-            description: ``,
+            description: `The carrier group is limited by only one thing—the presence of a divert airfield nearby. If the carrier were to sustain damage during combat, fighters in the air would find themselves at extreme risk without the presence of a divert airfield within range. Chinese companies have been involved in a number of airport projects that have been theorized to potentially allow future access, but the four runways in the spratlys serve as permanent, reliable airfields further from the Chinese mainland.`,
             location: {
-                center: [-83.677347, 37.629440],
+                center: [110.09, 22.30],
                 zoom: 5.23,
                 pitch: 52.00,
                 bearing: 13.88,
@@ -237,12 +237,12 @@ var config = {
         },
         {
             id: 'inDepth7',
-            title: '',
+            title: '[carrier group near natunas. Sam range, asm range, sensing range.]',
             subtitle: '',
             image: '',
-            description: ``,
+            description: `Fighter aircraft can operate from a carrier group under the radar coverage of the ships themselves, but the range is limited.`,
             location: {
-                center: [-82.15448, 34.74736],
+                center: [110.09, 22.30],
                 zoom: 6.72,
                 pitch: 2.00,
                 bearing: 0.00
@@ -263,12 +263,12 @@ var config = {
         },
         {
             id: 'inDepth8',
-            title: '',
+            title: '[KJ 500 over the carrier group, radar range]',
             subtitle: '',
             image: '',
-            description: ``,
+            description: `But a KJ-500 from the islands can make use the carrier group’s missile coverage to safely provide a much wider radar radius. This extends the effective combat range of the fighters by x miles, making it possible to strike locations as far as INDONESIA? under protected radar coverage.`,
             location: {
-                center: [-81.769179, 35.236806],
+                center: [110.09, 22.30],
                 zoom: 16,
                 pitch: 41,
                 bearing: 22.40,
@@ -302,12 +302,12 @@ var config = {
         },
         {
             id: 'inDepth9',
-            title: '',
+            title: '[divert airfields]',
             subtitle: '',
             image: '',
-            description: ``,
+            description: `Based on divert airfields currently available for Chinese use…`,
             location: {
-                center: [-80.638729, 36.357558],
+                center: [110.09, 22.30],
                 zoom: 8.16,
                 pitch: 41.00,
                 bearing: 16.30,
@@ -336,12 +336,12 @@ var config = {
         },
         {
             id: 'inDepth11',
-            title: '',
+            title: '[maximum KJ-500 sensing range from carriers]',
             subtitle: ``,
             image: '',
-            description: ``,
+            description: `China’s carrier groups can enable fighter combat operations within this range.`,
             location: {
-                center: [-83.685, 43.038],
+                center: [110.09, 22.30],
                 zoom: 16.30,
                 pitch: 50.00,
                 bearing: 0,
@@ -380,7 +380,7 @@ var config = {
             image: '',
             description: ``,
             location: {
-                center: [-77.57209, 43.19292],
+                center: [110.09, 22.30],
                 zoom: 8.07,
                 pitch: 55.00,
                 bearing: 41.60
@@ -413,7 +413,7 @@ var config = {
             image: '',
             description: ``,
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 4.00,
                 pitch: 0.00,
                 bearing: 0.00
@@ -438,7 +438,7 @@ var config = {
             image: '',
             description: `Twelve of those counties had both overall employment growth and an increase in manufacturing jobs as a percent of all jobs. Ten of those counties are situated in the Midwest. Some counties, like Marathon County, WI or Shelby County, OH, manufacture a diverse range of products. Others, like Elkhart County, IN, dominate production of a single item, which in Elkhart's case is RVs.`,
             location: {
-                center: [-86.962814, 42.775003],
+                center: [110.09, 22.30],
                 zoom: 6.06,
                 pitch: 0.00,
                 bearing: 0.00,
@@ -463,7 +463,7 @@ var config = {
             image: '',
             description: `The aerospace industry contributes the most to Snohomish County’s economic success, employing 46 percent of its manufacturing workers, with over 30,000 working for Boeing alone at their Everett Factory. The largest building in the world, Boeing produces the 747, 767, 777, and 787 models here. Boeing and its suppliers have undergirded the county’s economy for decades.`,
             location: {
-                center: [-122.27860, 47.91688],
+                center: [110.09, 22.30],
                 zoom: 14.00,
                 pitch: 60.00,
                 bearing: -28.80
@@ -506,7 +506,7 @@ var config = {
         {
             id: 'blank',
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 4.20,
                 pitch: 0.00,
                 bearing: 0.00
@@ -522,7 +522,7 @@ var config = {
         {
             id: 'close',
             location: {
-                center: [-98.82517, 41.00],
+                center: [110.09, 22.30],
                 zoom: 3.50,
                 pitch: 0.00,
                 bearing: 0.00
