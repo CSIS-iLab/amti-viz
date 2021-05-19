f9de79c344c5755581af9d79d4b5eb5ce51ad0e5
Serven Maraghi <maraghiserven@gmail.com>
2021-05-19 14:41:55 -0400

    Add new build file with removed manifest.json

---
+++ range-rings-scrolly/build/precache-manifest.175fb32d8136757d43626b09d24be9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41a92968be36bdd3bf4365a94f5b178",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72af1dada4ca90153faa",
+    "url": "./static/css/main.914a7e3e.chunk.css"
+  },
+  {
+    "revision": "eb803c9360f8979835d0",
+    "url": "./static/js/2.d31b04cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.d31b04cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72af1dada4ca90153faa",
+    "url": "./static/js/main.2f4374cd.chunk.js"
+  },
+  {
+    "revision": "f03cd37be987d7dbef45",
+    "url": "./static/js/runtime-main.4b1c9748.js"
+  }
+]);
--- range-rings-scrolly/build/service-worker.js
+++ range-rings-scrolly/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1549918ba9e0937fdbff37ea82bf576d.js"
+  "./precache-manifest.175fb32d8136757d43626b09d24be9ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
