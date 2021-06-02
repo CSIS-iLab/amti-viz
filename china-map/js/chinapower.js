bf0b70ccf93691f721cddc6e554c1fff7b252472
Serven Maraghi <maraghiserven@gmail.com>
2021-06-02 11:00:39 -0400

    Commit console logs for testing purposes

--- china-map/js/chinapower.js
+++ china-map/js/chinapower.js
@@ -57,10 +57,12 @@ _asyncToGenerator(
                 const queryString = window.location.search
                 const urlParams = new URLSearchParams(queryString)
                 lang = urlParams.get('lang')
+                console.log(lang, 'lang')
 
 
                 var suffix = lang ? "_" + lang : "";
                 suffix = suffix.replace("-", "_");
+                console.log(suffix, 'suffix')
                 var name = feature.properties["name" + suffix];
                 var description = feature.properties["description" + suffix];
                 var outpost = feature.properties.chinese_outposts;
