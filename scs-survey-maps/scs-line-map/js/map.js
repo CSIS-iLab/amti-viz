dc4a7105ab49a9cd87ddf392fd666650e5373489
Serven Maraghi <maraghiserven@gmail.com>
2022-01-28 13:06:14 -0500

    Init map structure

---
+++ scs-survey-maps/scs-line-map/js/map.js
@@ -0,0 +1,89 @@
+var url =
+  window.location != window.parent.location
+    ? document.referrer
+    : document.location.href;
+
+var href = /lang=([^&]+)/.exec(url);
+var lang = href ? href[1] : null;
+
+var basemap;
+if (lang && lang.indexOf("zh-") > -1) {
+  basemap = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/ilabmedia/citui3waw00162jo1zcsf1urj/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw",
+    {}
+  );
+} else {
+  basemap = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/ilabmedia/cj84s9bet10f52ro2lrna50yg/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw",
+    {}
+  );
+}
+
+var map = L.map("map", {
+  center: [13.7237264, 110.6814572],
+  zoom: 5,
+  maxZoom: 6,
+  scrollWheelZoom: true,
+  minZoom: 4,
+  zoomControl: true,
+  scrollWheelZoom: true,
+  layers: [basemap],
+  attributionControl: false,
+});
+
+const client = new carto.Client({
+  apiKey: "YctydLsZy6l2cnqMBagE5A",
+  username: "csis",
+});
+
+const mapSource = new carto.source.SQL(`SELECT * FROM 
+scs_surverys_line`);
+
+const mapStyle = new carto.style.CartoCSS(`#layer {
+  line-width: 1.5;
+  line-color: #4CC8A3;
+  line-opacity: 1;
+}`);
+
+const mapLayer = new carto.layer.Layer(mapSource, mapStyle, {
+  featureOverColumns: ["date", "end_date", "toggle", "layer"],
+});
+
+client.addLayer(mapLayer);
+
+client.getLeafletLayer().bringToFront().addTo(map);
+
+const popup = L.popup({ closeButton: true });
+
+mapLayer.on(carto.layer.events.FEATURE_CLICKED, createPopup);
+
+function createPopup(event) {
+  popup.setLatLng(event.latLng);
+
+  if (!popup.isOpen()) {
+    var data = event.data;
+    console.log(event.data);
+    var content = "<div>";
+
+    content += `
+    <div class="popupHeaderStyle">
+      ${data.layer}
+    </div>
+    <div class="popupEntryStyle">
+      ${data.date}
+      ${data.end_date}
+    </div>
+    `;
+    popup.setContent("" + content);
+    popup.openOn(map);
+  }
+}
+
+L.control
+  .attribution({
+    position: "bottomright",
+  })
+  .setPrefix(
+    '<a href="https://www.csis.org/programs/PROGRAMNAME">CSIS PROGRAM</a>, <a href="https://leafletjs.com/">Leaflet</a>'
+  )
+  .addTo(map);
