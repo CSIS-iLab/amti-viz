d7915efa33c4f97dddb1f8a881ddc8058c66f834
Jose Romero <jdromerov88@gmail.com>
2023-01-19 12:32:15 -0500

    Fetching from google spreadsheet to automate the timeline

--- coast-guard-timeline-2022/js/index.js
+++ coast-guard-timeline-2022/js/index.js
@@ -1,57 +1,32 @@
-let DATA_FORMATTED = []
-let test = []
+const DATA_FORMATTED = []
 const URL = `https://content-sheets.googleapis.com/v4/spreadsheets/1BfIDHH5pwXrk-qDdAcBDL_vhClfl3xFRHymJBjl_Z-I/values/Sheet1?key=AIzaSyBXuQRRw4K4W8E4eGHoSFUSrK-ZwpD4Zz4&majorDimension=ROWS`;
 
 async function getData() {
   const RESPONSE = await fetch(URL)
   const DATA = await RESPONSE.json()
-  return formatData(DATA.values).slice(0, 2)
+  return formatData(DATA.values).slice(0, 15)
 }
 
 function formatData(data) {
-  // console.log(data)
   data.shift()
   dataLength = data.length
   for (let index = 0; index < dataLength; index++) {
-    let element = data[index]
-    console.log(element)
-    // console.log(Array.isArray(element))
-    element.map( (elem, i ) => {
-      console.log(elem);
-      let test;
-      if ( i > 1 ) {
-        test = new Date(elem)
-        console.log(test)
-        return test
+    let row = []
+    data[index].forEach((element, i) => {
+      if (i > 1) {
+        row.push(new Date(element))
+      } else {
+        row.push(element)
       }
-      test = elem
-      console.log(test)
-      return test
     })
-    // return element
-    // data.map((innerElem, index) => {
-    //   let element;
-    //   if (index > 1) {
-    //     element = new Date(innerElem);
-    //     console.log(element);
-    //   }
-    //   element = innerElem;
-    //   return element;
-    // });
+    DATA_FORMATTED[index] = row
   }
-  return data
-  // return DATA_FORMATTED
+  return DATA_FORMATTED
 }
 
 google.charts.load("current", { packages: ["timeline"] });
-// google.charts.setOnLoadCallback(drawChart);
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  // DATA_FORMATTED = getData()
-  // if (DATA_FORMATTED) {
-  //   console.log('houston we have data!');
-  //   console.log(getData())
-  // }
   google.charts.setOnLoadCallback(drawChart)
 })
 
@@ -65,28 +40,7 @@ async function drawChart() {
   dataTable.addColumn({ type: "string", id: "Name" });
   dataTable.addColumn({ type: "date", id: "Start" });
   dataTable.addColumn({ type: "date", id: "End" });
-  console.log(dataFormatted)
   dataTable.addRows(dataFormatted)
-  // dataTable.addRows([
-  //   [
-  //     "Second Thomas Shoal",
-  //     "Haijing 5402",
-  //     new Date(2020, 0, 6),
-  //     new Date(2020, 1, 14),
-  //   ],
-  //   [
-  //     "Second Thomas Shoal",
-  //     "Haijing 5402",
-  //     new Date(2020, 1, 16),
-  //     new Date(2020, 1, 26),
-  //   ],
-  //   [
-  //     "Second Thomas Shoal",
-  //     "Haijing 5402",
-  //     new Date(2020, 3, 10),
-  //     new Date(2020, 4, 23),
-  //   ]
-  // ]);
 
   var options = {
     colors: ["#58a897", "#83badc", "#3b75bb", "#c79bd3"],
