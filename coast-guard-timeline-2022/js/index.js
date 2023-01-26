9f62b5ba1c85b25e88488a49a18120b05e062c18
jdromero88 <jdromerov88@gmail.com>
2023-01-26 09:21:55 -0500

    Add styles.css, working on calculating the duration for the tooltip

--- coast-guard-timeline-2022/js/index.js
+++ coast-guard-timeline-2022/js/index.js
@@ -35,6 +35,7 @@ async function drawChart() {
   var container = document.getElementById("timeline-tooltip");
   var chart = new google.visualization.Timeline(container);
   var dataTable = new google.visualization.DataTable();
+  // var dateFormat = new google.visualization.DateFormat({ pattern: 'd.M.yy' })
 
   dataTable.addColumn({ type: "string", id: "Location" });
   dataTable.addColumn({ type: "string", id: "Name" });
@@ -42,8 +43,51 @@ async function drawChart() {
   dataTable.addColumn({ type: "date", id: "End" });
   dataTable.addRows(dataFormatted)
 
+  // pseudo code
+  // show the duration in this format: Duration: xx month, xx days
+  dataTable.insertColumn(2, {
+    type: "string",
+    role: "tooltip",
+    p: { html: true },
+  })
+
+  var dateFormat = new google.visualization.DateFormat({
+    formatType: 'medium'
+  })
+
+  for (var i = 0; i < dataTable.getNumberOfRows(); i++) {
+    var duration =
+      (
+        dataTable.getValue(i, 4).getTime() - dataTable.getValue(i, 3).getTime()
+      ) / 1000
+      console.log(duration)
+    // duration = new Date(duration).toString()
+    var days = parseInt(duration / (3600*24))
+    var hours = parseInt(duration / 3600) % 24;
+    var minutes = parseInt(duration / 60) % 60;
+    var seconds = duration % 60;
+    console.log(days, 'days', hours, 'hours', minutes,'minutes');
+    var tooltip =
+      '<div class="ggl-tooltip"><span>' +
+      dataTable.getValue(i, 1) +
+      '</span></div><div class="ggl-tooltip"><span>' +
+      dataTable.getValue(i, 0) +
+      "</span>: " +
+      dateFormat.formatValue(dataTable.getValue(i, 3)) +
+      " - " +
+      dateFormat.formatValue(dataTable.getValue(i, 4)) +
+      "</div>" +
+      '<div class="ggl-tooltip"><span>' +
+      'Duration' +
+      '</span>: ' + duration  +
+      '</div>';
+
+    dataTable.setValue(i, 2, tooltip);
+  }
+
   var options = {
-    colors: ["#58a897", "#83badc", "#3b75bb", "#c79bd3"],
+    // colors: ["#58a897", "#83badc", "#3b75bb", "#c79bd3", "#FCF1AB"],
+    colors: ["#58a897", "#83badc", "#3b75bb", "#c79bd3", "#ca6d92"],
     timeline: {
       rowLabelStyle: {
         fontName: "Arial",
@@ -53,7 +97,10 @@ async function drawChart() {
       showBarLabels: false,
       colorByRowLabel: true,
     },
-  };
+    tooltip: {
+      isHtml: true,
+    },
+  }
 
   chart.draw(dataTable, options);
 }
